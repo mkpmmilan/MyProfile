dd6cafff8bd5290ba309858fd6e179afaa0a7428
mkpmmilan <prageethmilan99@gmail.com>
2022-02-19 09:52:04 +0530

    Add Home page JS

--- myprojects/SPA/assets/js/script.js
+++ myprojects/SPA/assets/js/script.js
@@ -1,30 +1,3 @@
-$(".naviItem-1").click(function () {
-    $("#carouselExampleIndicators").css('display', 'block');
-    $("#customerPage").css('display', 'none');
-    $("#itemPage").css('display', 'none');
-    $("#placeOrderPage").css('display', 'none');
-});
-
-$(".naviItem-2").click(function () {
-    $("#carouselExampleIndicators").css('display', 'none');
-    $("#customerPage").css('display', 'block');
-    $("#itemPage").css('display', 'none');
-    $("#placeOrderPage").css('display', 'none');
-});
-
-$(".naviItem-3").click(function () {
-    $("#carouselExampleIndicators").css('display', 'none');
-    $("#customerPage").css('display', 'none');
-    $("#itemPage").css('display', 'block');
-    $("#placeOrderPage").css('display', 'none');
-});
-
-$(".naviItem-4").click(function () {
-    $("#carouselExampleIndicators").css('display', 'none');
-    $("#customerPage").css('display', 'none');
-    $("#itemPage").css('display', 'none');
-    $("#placeOrderPage").css('display', 'block');
-});
 
 $(".customerHomeNavi").click(function () {
     $("#carouselExampleIndicators").css('display', 'block');
---
+++ myprojects/SPA/controller/HomeController.js
@@ -0,0 +1,27 @@
+$(".naviItem-1").click(function () {
+    $("#carouselExampleIndicators").css('display', 'block');
+    $("#customerPage").css('display', 'none');
+    $("#itemPage").css('display', 'none');
+    $("#placeOrderPage").css('display', 'none');
+});
+
+$(".naviItem-2").click(function () {
+    $("#carouselExampleIndicators").css('display', 'none');
+    $("#customerPage").css('display', 'block');
+    $("#itemPage").css('display', 'none');
+    $("#placeOrderPage").css('display', 'none');
+});
+
+$(".naviItem-3").click(function () {
+    $("#carouselExampleIndicators").css('display', 'none');
+    $("#customerPage").css('display', 'none');
+    $("#itemPage").css('display', 'block');
+    $("#placeOrderPage").css('display', 'none');
+});
+
+$(".naviItem-4").click(function () {
+    $("#carouselExampleIndicators").css('display', 'none');
+    $("#customerPage").css('display', 'none');
+    $("#itemPage").css('display', 'none');
+    $("#placeOrderPage").css('display', 'block');
+});
